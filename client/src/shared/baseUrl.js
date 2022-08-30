5fb139efd854c1fd92a0cccb70c3ebe700a02210
Patch Kientz <patch@sphere.com.ai>
2022-08-30 16:28:57 -0700

    first commit

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'http://13.56.191.168/'; //will need to update if you stop and re-start ec2 instance
+export const baseUrl = 'https://localhost:5443/'; //will need to update if you stop and re-start ec2 instance
+//http://13.56.191.168/
