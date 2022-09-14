1ec9dc832314a5bda45fb8eaa12a4aed455c35c5
Patrick "Patch" Kientz <patchkientz@gmail.com>
2022-09-14 13:19:32 -0700

    production env var changes

--- server/routes/cors.js
+++ server/routes/cors.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 
-const whitelist = ['http://localhost', 'http://3.101.198.85', 'http://localhost:3000'];
+const whitelist = ['http://localhost', 'http://3.101.198.85', 'http://localhost:3000', 'https://3.101.198.85'];
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
     console.log(req.header('Origin'));
